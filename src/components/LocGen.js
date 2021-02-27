0ea403c1f5726890d7256f72de459614a3a0447f
Daniel Mullaney <danftv@gmail.com>
2021-02-27 17:05:54 -0500

    saturday experiments

--- src/components/LocGen.js
+++ src/components/LocGen.js
@@ -5,9 +5,10 @@ import PlayButtons from "./PlayButtons";
 import { useState } from "react";
 
 function LocationGenerator(props) {
-  // const [randomPosition, setRandomPosition] = useState()
+  
+  const [mapPosition, setMapPosition] = useState()
 
-  // const map = useMap()
+  const [zoom, setZoom] = useState()
 
   let minLat = 42.730315121762715;
   let maxLat = 45.007561302382754;
@@ -25,41 +26,36 @@ function LocationGenerator(props) {
   //this needs to live outside the function for it to be used!!
   let randomLat = randomSpot(minLat, maxLat);
   let randomLong = randomSpot(minLong, maxLong);
-
+  
   //Create Consistent Randomized Location Variable
-  function SetRandomPosition() {
-
+  function setRandomPosition() {
+    
     //Returns an array. if length is 0, not in VT, goes again. if length is 1, it is in VT
     let inBounds = leafletPip.pointInLayer([randomLong, randomLat], gjLayer);
 
     //If In VT Change the Zoom and View
-    if (inBounds.length === 1) {
-    //   const map = UseMapEvent("click", () => {
-    //     map.setView([randomLat, randomLong], 18);
-    //   });
-    //   return null;
-    // } else {
+    if (inBounds.length === 1) {  
+      return [randomLong, randomLat] 
+    } else {
+      
       //if not in VT loop over until it equals 1
       while (inBounds.length === 0) {
         randomLat = randomSpot(minLat, maxLat);
         randomLong = randomSpot(minLong, maxLong);
         inBounds = leafletPip.pointInLayer([randomLong, randomLat], gjLayer);
 
-        //If In VT Change the Zoom and View
-        // if (inBounds.length === 1) {
-        //   const map = UseMapEvent("click", () => {
-        //     map.setView([randomLat, randomLong], 18);
-        //   });
-        //   return null;
-        // }
+        // If In VT Change the Zoom and View
+        if (inBounds.length === 1) {
+          return [randomLong, randomLat]
+        }
       }
     }
   }
 
   return (
-    <MapContainer view={[randomLat, randomLong]} zoom={18}>
-      <PlayButtons />
-    </MapContainer>
+    <div>
+      <PlayButtons setRandomPosition={setRandomPosition}/>
+    </div>
   );
 }
 
